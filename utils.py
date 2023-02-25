0bb47fc05eb08b8b6dc6d8646e338d6a8d8c6999
Sushmita Chandel <75020246+SushmitaChandel@users.noreply.github.com>
2023-02-25 16:03:34 +0530

    utils.py
    It is a utility file to perform simple calculations

---
+++ utils.py
@@ -0,0 +1,24 @@
+def add(a,b):
+  return a+b
+
+def subtract(a,b):
+  return a-b
+
+def quo(a,b):
+  if b != 0:
+    return a//b
+  else
+    return -1
+
+def rem(a,b):
+  if b != 0:
+    return a%b
+  else:
+    return -1
+
+def divide(a,b):
+  if b != 0:
+    return a/b
+  else:
+    return -1
+  
